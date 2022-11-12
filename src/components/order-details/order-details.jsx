c4976c5d503c5753831d83a98a7086f95a2c9725
IliaGorin <101461772+IliaGorin@users.noreply.github.com>
2022-11-12 17:49:31 +0300

    add order details

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,9 +1,32 @@
 import React, { useState } from 'react';
 import stylesForOrderDetails from './order-details.module.css';
 import PropTypes from 'prop-types';
+import imageOrderDone from '../../images/done.svg';
 
 const OrderDetails = (props) => {
-  return <div>Test</div>;
+  return (
+    <div className={`${stylesForOrderDetails.orderDetailsWrapper}`}>
+      <p
+        className={`${stylesForOrderDetails.orderNumber} text text_type_digits-large mt-20 mb-8`}
+      >
+        034536
+      </p>
+      <p className="text text_type_main-medium text_color_primary ">
+        идентификатор заказа
+      </p>
+      <img
+        className={`${stylesForOrderDetails.orderDetailsIcon} mt-15 mb-15`}
+        src={imageOrderDone}
+        alt="alt"
+      />
+      <p className="text text_type_main-default text_color_primary">
+        Ваш заказ начали готовить
+      </p>
+      <p className="text text_type_main-default text_color_inactive mt-2 mb-20">
+        Дождитесь готовности на орбитальной станции
+      </p>
+    </div>
+  );
 };
 
 // OrderDetails.propTypes = {
